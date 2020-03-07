58225546cfc48e7fc076bcfdaa8dcc78faf1fb56
李璐 <44078019+Katherine-LiLu@users.noreply.github.com>
2020-03-07 22:41:34 +0800

    Add files via upload

--- each_xlsx_to_one.py
+++ each_xlsx_to_one.py
@@ -9,19 +9,21 @@ from tqdm import tqdm
 def find_miss_post(filepath):
     filename_list = []
     for file in os.listdir(filepath):
-        filename_list.append(int(file.split('.')[0]))
+        if file.endswith('xlsx'):
+            filename_list.append(int(file.split('.')[0]))
     filename_list.sort()
     max_post = filename_list[-1]
     for ele in range(0, max_post):
         if (ele+1) not in filename_list:
-            print(ele)
+            print(str(ele)+' is not exist')
 
 
 # 获取目录下的所有文件名
 def get_filename(tar_path):
     filename_list = []
     for file in os.listdir(tar_path):
-        filename_list.append(int(file.split('.')[0]))
+        if file.endswith('xlsx'):
+            filename_list.append(int(file.split('.')[0]))
     filename_list.sort()
     filename = []
     for file in filename_list:
@@ -37,11 +39,13 @@ def concat_and_insert(f_dir):
         data = xlrd.open_workbook(ai)
         # 第一个sheet页的名称
         first_sheet = data.sheet_by_index(0).name
-        print(ai, '>'*10, first_sheet)
+        # print(ai, '>'*10, first_sheet)
         # 获取sheet页的名称
         sheet = data.sheet_by_name(first_sheet)
         # 获取表的行数
         n_rows = sheet.nrows
+        if n_rows == 0:
+            print(ai+' is empty')
         for i in range(n_rows):
             records.append(sheet.row_values(i))
     return records
@@ -62,9 +66,9 @@ if __name__ == '__main__':
 
     find_miss_post(file_path)
 
-    filename = get_filename(file_path)
+    file_name = get_filename(file_path)
 
-    records_all = concat_and_insert(filename)
+    records_all = concat_and_insert(file_name)
 
     tarname = file_path
     write_file(records_all, tarname)
